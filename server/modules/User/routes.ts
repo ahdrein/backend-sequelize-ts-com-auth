245cd5bad1b04369af40bb17fca30f70718f46b7
Andrews H. Drein <andrews.drein@thomsonreuters.com>
2017-10-02 22:02:47 -0300

    Deploy

--- server/modules/User/routes.ts
+++ server/modules/User/routes.ts
@@ -6,24 +6,24 @@ class UserRoutes {
 
   constructor() { }
 
-  index(req: Request, res: Response) {
-    return UserController.getAll(req, res);
+  async index(req: Request, res: Response): Bluebird<void> {
+    return await UserController.getAll(req, res);
   }
 
-  create(req: Request, res: Response) {
-    return UserController.createUser(req, res);
+  async create(req: Request, res: Response): Bluebird<void> {
+    return await UserController.createUser(req, res);
   }
 
-  findOne(req: Request, res: Response) {
-    return UserController.getById(req, res);
+  async findOne(req: Request, res: Response): Bluebird<void> {
+    return await UserController.getById(req, res);
   }
 
-  update(req: Request, res: Response) {
-    return UserController.updateUser(req, res);
+  async update(req: Request, res: Response): Bluebird<void> {
+    return await UserController.updateUser(req, res);
   }
 
-  destroy(req: Request, res: Response) {
-    return UserController.deleteUser(req, res);
+  async destroy(req: Request, res: Response): Bluebird<void> {
+    return await UserController.deleteUser(req, res);
   }
 
 }
